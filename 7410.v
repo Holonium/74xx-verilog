facb4e8ebb49c606e08e7038dafc80ce56c970cf
Holonium <holonium2.0@gmail.com>
2020-12-12 14:30:48 -0800

    Removed excess comma from 7410

--- 7410.v
+++ 7410.v
@@ -1,7 +1,7 @@
 module mod_7408 (
 	input A1, input B1, input C1, output Y1, 
 	input A2, input B2, input C2, output Y2, 
-	input A3, input B3, input C3, output Y3, 
+	input A3, input B3, input C3, output Y3 
 );
 	assign Y1 = !(A1 & B1 & C1);
 	assign Y2 = !(A2 & B2 & C2);
